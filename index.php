9f6385d42031a24ee433c40489aac32802abacd1
Andrew Afonso <andrewbluepiano@hotmail.com>
2020-04-16 15:15:47 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// Control page for SignPi
+?>
+<!doctype html>
+<head>
+	<title>SignPi Controller</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="initial-scale=1">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<form method="POST" action="scripts/lightcontrol.php">
+		<fieldset>
+			<legend>Light Controls</legend>
+			<label>Color: <input type="color" id="lightcolor" name="lightcolor" value="#ff0000"></label>
+			<label>Brightness: <input type="range" id="brightness" name="brightness" min="0" max="225"></label>
+		</fieldset>
+	</form>
+</body>
+<footer>
+
+</footer>
+</html>
