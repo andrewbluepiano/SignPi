1a84bd4f20ec9497e7ea2966f5b1208ef2758231
Andrew Afonso <andrewbluepiano@hotmail.com>
2020-04-17 19:04:20 -0400

    Update lightPiSetup.sh

--- lightPiSetup.sh
+++ lightPiSetup.sh
@@ -17,3 +17,5 @@ make
 sudo make install
 (crontab -l; echo '@reboot sudo pigpiod' ) | crontab -
 sudo wget -O /var/www/html/index.php https://raw.githubusercontent.com/andrewbluepiano/SignPi/master/index.php
+sudo mkdir /var/www/html/css/
+sudo wget -O /var/www/html/css/style.css https://raw.githubusercontent.com/andrewbluepiano/SignPi/master/style.css
