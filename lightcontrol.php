a03460ec260989e8f0ce53e01559b717300be48f
Andrew Afonso <andrewbluepiano@hotmail.com>
2020-04-17 23:15:30 -0400

    Update and rename controller.php to lightcontrol.php

--- lightcontrol.php
+++ lightcontrol.php
@@ -1,5 +1,5 @@
 <?php
 // Author: Andrew Afonso
 // Description: A simlpe controller that sets the colors on a GPIO controlled LED strip on a RasPi
-echo $_POST['color'];
+echo $_POST['lightcolor'];
 ?>
